21d90056f7f84dae8c6875a4d765e87e3ac4067d
atammara <mtasawor68@gamil.com>
2024-06-20 06:02:46 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+#! /usr/bin/env node
+class Bankaccount {
+    accountBalance;
+    constructor(accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+    credit(amount) {
+        if (amount > 0) {
+            this.accountBalance += amount;
+            console.log("Credit sucessfully get into new account and your balance is:" + this.accountBalance);
+        }
+        else {
+            console.log("credit unsucessful");
+        }
+    }
+    debit(amount) {
+        if (amount > 0 && amount < this.accountBalance) {
+            this.accountBalance -= amount;
+            console.log("debit successfully share in new account balance:" + this.accountBalance);
+        }
+        else {
+            console.log("debit unsuccessfully");
+        }
+    }
+}
+class Customer {
+    person;
+    age;
+    gender;
+    mobile_number;
+    bankaccount;
+    constructor(person, age, gender, mobile_number, bankaccount) {
+        this.person = person;
+        this.age = age;
+        this.gender = gender;
+        this.mobile_number = mobile_number;
+        this.bankaccount = bankaccount;
+    }
+    display() {
+        console.log("\nName:" + this.person.firstName + "" + this.person.lastName);
+        console.log("Age:" + this.age);
+        console.log("Gender:" + this.gender);
+        console.log("Mobile Number:" + this.mobile_number);
+        console.log("Amount In Bank:" + this.bankaccount.accountBalance);
+        console.log();
+    }
+}
+const a1 = new Bankaccount(8000);
+const c1 = new Customer({ firstName: "ammara", lastName: "tasawwur" }, 22, "female", 3012829293, a1);
+c1.display();
+c1.bankaccount.debit(500);
+console.log();
+const a2 = new Bankaccount(5000);
+const c2 = new Customer({ firstName: "hani", lastName: "tasawwur" }, 18, "female", 301287232, a2);
+c2.display();
+c2.bankaccount.debit(1000);
+console.log();
+export {};
